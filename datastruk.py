6f8e3014bdd423aa2fb3de53f500ab321620b3d2
msfasaladin <100893296+msfasaladin@users.noreply.github.com>
2024-03-01 10:28:55 +0700

    Add files via upload

---
+++ datastruk.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+st.title('Data Struk Crisbar')
+
+Pemesanan = st.selectbox('Jenis Pemesanan', ['Makan di Tempat', 'Take Away'])
+if Pemesanan == 'Makan di Tempat' :
+    JenisKelamin = st.selectbox('Jenis Kelamin Pelanggan', ['Laki-Laki', 'Perempuan'])
+if Pemesanan == 'Take Away' :
+    Metode = st.selectbox('Metode Pemesanan', ['Datang Langsung', 'On Demand Services'])
+    if Metode == 'On Demand Services' :
+        st.selectbox('Jenis On Demand Services', ['Go Food', 'Grab Food', 'Shopee Food'])
+    else :
+        st.selectbox('Jenis Kelamin Pelanggan', ['Laki-Laki', 'Perempuan'])
+Pembayaran = st.selectbox('Metode Pembayaran', ['Tunai', 'Cashless'])
+if Pembayaran == 'Cashless' :
+    st.selectbox('Jenis Pembayaran Cashless', ['Debit', 'Ovo', 'Dana', 'Gopay', 'Shopeepay'])
+st.date_input('Tanggal Pembuatan Struk')
+st.time_input('Waktu Pembuatan Struk')
